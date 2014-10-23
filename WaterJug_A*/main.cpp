baf37abee7da43524e8e967e95f256ef85497ed1
Liza Girsova <Liza Girsova@fondowt-1.student.lawrence.edu>
2014-10-22 20:02:27 -0500

    Initial Commit

---
+++ WaterJug_A*/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WaterJug_A*
+//
+//  Created by Liza Girsova on 10/22/14.
+//  Copyright (c) 2014 CMSC470. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
